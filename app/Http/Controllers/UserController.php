77955f28287ddaae889bc7a00893e6e71f61ebc9
Munawir Fikri <munawirfikri@gmail.com>
2023-02-12 20:51:56 +0700

    get data users api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Request;
+
 class UserController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+    public function getUsers(Request $request){
+        $users = DB::table('users')->get();
 
-    //
+        $response['error'] = false;
+        $response['message'] = "Success";
+        $response['total_data'] = count($users);
+        $response['data'] = $users;
+
+        return response()->json($response, 200);
+
+    }
 }
