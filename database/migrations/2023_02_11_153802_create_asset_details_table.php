baa99bea177293b0dc6ebce273882e2fa39e075a
Munawir Fikri <munawir.fikri@aryanoble.co.id>
2023-02-12 20:04:24 +0700

    first commit

---
+++ database/migrations/2023_02_11_153802_create_asset_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asset_details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('assets_id');
+            $table->string('url');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asset_details');
+    }
+};
